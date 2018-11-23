451ddad3422944d4e388bcf99c85518e17ecdc2c
Peter Kirk <codesmore@yahoo.com>
2018-11-23 17:56:51 -0500

    initial commit

---
+++ Assets/Scripts/Dialogue/IntroDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogue : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+	// Use this for initialization
+	void Start () {
+        dialogue.Name = "???";
+
+        dialogue.Sentences = new List<string>();
+        dialogue.Sentences.Add("Welcome to GoatRemover.exe. Would you kindly wait one moment while I prepare everything? Super...");
+        dialogue.Sentences.Add("While you're waiting, would you kindly enter the name of the most annoying person you know?");
+        dialogue.Sentences.Add("Like Don, that stupid carrot...");
+	}
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueController>().StartDialogue(dialogue);
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ProgressController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressController : MonoBehaviour {
+
+    private Image[] HighlightEdges { get; set; }
+
+    // Use this for initialization
+    void Start () {
+        HighlightEdges = GameObject.Find("HighlightEdges").GetComponentsInChildren<Image>();
+
+        HideHighlightEdges();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            FadeInHighlightEdges();
+        }
+	}
+
+    private void HideHighlightEdges()
+    {
+        foreach (var edge in HighlightEdges)
+        {
+            edge.color = new Color(edge.color.r, edge.color.g, edge.color.b, 0);
+        }
+    }
+
+    private void FadeInHighlightEdges()
+    {
+        foreach (var edge in HighlightEdges)
+        {
+            edge.color = new Color(edge.color.r, edge.color.g, edge.color.b, 1);
+        }
+    }
+}
